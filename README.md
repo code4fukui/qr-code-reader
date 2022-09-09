872659a660355021d86fc3051f282a4dab7a2459
Taisuke Fukuno <fukuno@jig.jp>
2022-09-10 08:14:41 +0900

    add qr-code-reader

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # qr-code-reader
- 
+
+## demo
+
+- [demo](https://code4fukui.github.io/qr-code-reader/)
+
+## dependencies
+
+- [Camera.js](https://github.com/code4fukui/Camera/)
+- [jsQR](https://github.com/code4fukui/jsQR-es/)
+
+## related
+
+- [qr-code](https://github.com/code4fukui/qr-code/)
+
---
+++ qr-code-reader.js
@@ -0,0 +1,63 @@
+import { Camera } from "https://code4fukui.github.io/Camera/Camera.js";
+import { jsQR } from "https://code4fukui.github.io/jsQR-es/jsQR.js";
+
+class QRCodeReader extends HTMLElement {
+  constructor() {
+    super();
+    const btn = document.createElement("button");
+    btn.textContent = "QRコード読み込み";
+    btn.style.margin = "2em";
+    btn.style.padding = "3em";
+    this.appendChild(btn);
+    this.btn = btn;
+
+    const canvas = document.createElement("canvas");
+    canvas.style.maxWidth = "100%";
+    this.appendChild(canvas);
+    const g = canvas.getContext("2d");
+    canvas.style.display = "none";
+    this.canvas = canvas;
+
+    btn.onclick = async () => {
+      btn.style.display = "none";
+      canvas.style.display = "inline-block";
+      
+      const video = document.createElement("video");
+
+      const onFrame = async () => {
+        canvas.width = video.videoWidth;
+        canvas.height = video.videoHeight;
+        g.drawImage(video, 0, 0, canvas.width, canvas.height);
+        const imageData = g.getImageData(0, 0, canvas.width, canvas.height);
+        const code = jsQR(imageData.data, imageData.width, imageData.height, {
+          inversionAttempts: "dontInvert",
+        });
+        if (code) {
+          this.oninput(code);
+        }
+      };
+      this.camera = new Camera(video, {
+        onFrame,
+        width: 1280,
+        height: 720,
+        backcamera: true,
+      });
+      this.camera.start();
+      this.canvas = canvas;
+    };
+  }
+  oninput(bin) {
+    console.log("oninput", bin);
+  }
+  stop() {
+    if (this.camera) {
+      this.camera.stop();
+    }
+    this.btn.style.display = "inline-block";
+    this.canvas.style.display = "none";
+  }
+};
+
+customElements.define("qr-code-reader", QRCodeReader);
+
+export { QRCodeReader };
