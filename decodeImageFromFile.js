4ff033bf84d849ec7f8029c521a9289699ffa1b9
Taisuke Fukuno <fukuno@jig.jp>
2024-11-17 11:24:37 +0900

    support PNG

--- decodeImageFromFile.js
+++ decodeImageFromFile.js
@@ -3,12 +3,16 @@ import { JPEG } from "https://taisukef.github.io/jpeg-js-es/JPEG.js";
 import { PNG } from "https://taisukef.github.io/PNG/PNG.js";
 
 export const decodeImageFromFile = async (file) => {
-  const bin = await readAsArrayBufferAsync(file.file);
-  if (file.file.name.toLowerCase().endsWith(".png")) {
-    const img = PNG.decode(new Uint8Array(bin));
+  try {
+    const bin = await readAsArrayBufferAsync(file.file);
+    if (file.file.name.toLowerCase().endsWith(".png")) {
+      const img = PNG.decode(new Uint8Array(bin));
+      return img;
+    }
+    //console.log(img);
+    const img = JPEG.decode(bin);
     return img;
+  } catch (e) {
+    return null;
   }
-  //console.log(img);
-  const img = JPEG.decode(bin);
-  return img;
 };
